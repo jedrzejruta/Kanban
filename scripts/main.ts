97e3568a81aec00796c97f0a7ce051e6a1fd7f40
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-04-26 23:01:22 +0200

    add displayBoards method add new elements to Board class make boards to be stored in Kanban class

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,21 +1,37 @@
 class Board  {
-	boardClassName: string = 'board';
-	mainPage: HTMLElement = <HTMLElement>document.querySelector('main');
-
+	private boardClassName: string = 'board'; //change and add appropriate properties to class
+	
 	constructor(public boardID: string) {
+		this.addNewBoard(boardID);
+	}
+
+	private addNewBoard(_boardID: string){
+		const mainPage: HTMLElement = <HTMLElement>document.querySelector('main');
 		const newBoardSection: HTMLElement = document.createElement('section');
 		const newBoardTitle: HTMLHeadingElement = document.createElement('h1');
 		const addTaskBtn: HTMLButtonElement = document.createElement('button');
-		addTaskBtn.innerText = 'Add task';
-		newBoardTitle.innerText = boardID;
+		const taskNameInput: HTMLInputElement = document.createElement('input');
+
+		taskNameInput.setAttribute('type', 'text');
+		taskNameInput.setAttribute('placeholder','Task list name');
+		taskNameInput.classList.add('taskListName');
+
+		addTaskBtn.innerText = 'Add task list';
+		addTaskBtn.id = 'taskBtn';
+
+		newBoardTitle.innerText = _boardID;
+
 		newBoardSection.classList.add(this.boardClassName);
+		newBoardSection.id = _boardID;
 		newBoardSection.appendChild(newBoardTitle);
+		newBoardSection.appendChild(taskNameInput);
 		newBoardSection.appendChild(addTaskBtn);
-
-		this.mainPage.appendChild(newBoardSection);
+		
+		mainPage.appendChild(newBoardSection);
 	}
-
 }
+//TODO:
+//Task list contains notes, add notes, drag and drop
 
 class Task {
 	constructor(public taskName: string) {
@@ -25,23 +41,34 @@ class Task {
 }
 
 class Kanban {
-	createBoard: HTMLButtonElement = <HTMLButtonElement>document.querySelector('#newBoard');
-	constructor(){
+	private createBoard: HTMLButtonElement = <HTMLButtonElement>document.querySelector('#newBoard');
+	private boards: Array<Board> = [];
 
+	constructor(){
+		this.addNewBoard();
+	}
+
+	private addNewBoard(){
 		this.createBoard.addEventListener('click',() => {
-			const _boardID: string = (<HTMLInputElement>document.querySelector('#boardId')).value;
-			//TODO:
-			//make function to validate input 
-			if(_boardID.trim().length === 0){
-				return false;
+			const _boardID: string = (<HTMLInputElement>document.querySelector('#boardId')).value.trim();
+			if(_boardID.length !== 0){
+				const board: Board = new Board(_boardID);
+				this.boards.push(board);
+				this.displayBoards(_boardID);
 			}
 			else {
-				new Board(_boardID.trim());
-				console.log(_boardID);
+				return;
 			}
 		});
 	}
 
+	displayBoards(_boardID: string){
+		const boardList: HTMLUListElement = <HTMLUListElement>document.querySelector('#boardList');
+		const boardListEl: HTMLLIElement = document.createElement('li');
+		const tempBoard: Array<Board> = this.boards.filter(el => el.boardID === _boardID);
+		boardListEl.textContent = tempBoard[0].boardID;
+		boardList.appendChild(boardListEl);
+	}
 }
 
 document.addEventListener('DOMContentLoaded',() => {

5ef3e0f9c105efc98cfbacb01705dcd15651db8d
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-05-30 16:53:20 +0200

    change list section style when dragging task

--- src/board.ts
+++ src/board.ts
@@ -10,11 +10,11 @@ export class Board  {
 	}
 
 	private addNewBoard(_boardName: string): void{
-	// TODO, W trakcie, Zrobione
 		const mainPage: HTMLElement = <HTMLElement>document.querySelector('main'),
 			taskForm: HTMLFormElement = document.createElement('form'),
 			addTaskBtn: HTMLButtonElement = document.createElement('button'),
 			newBoardSection: HTMLElement = document.createElement('section'),
+			boardInfoContainer: HTMLElement = document.createElement('article'),
 			newBoardTitle: HTMLHeadingElement = document.createElement('h1'),
 			taskNameInput: HTMLInputElement = document.createElement('input'),
 			taskDescInput: HTMLTextAreaElement = document.createElement('textarea');
@@ -30,28 +30,21 @@ export class Board  {
 		taskDescInput.setAttribute('placeholder','Write your note here');
 		taskDescInput.classList.add('taskListDesc');
 
-		addTaskBtn.innerText = 'Add task list';
+		addTaskBtn.textContent = 'Add task';
 		addTaskBtn.id = 'taskBtn';
 
-		newBoardTitle.innerText = _boardName;
+		newBoardTitle.textContent = _boardName;
 
 		newBoardSection.classList.add('board','active');
 		newBoardSection.id = this.boardID;
 		taskForm.appendChild(taskNameInput);
 		taskForm.appendChild(taskDescInput);
 		taskForm.appendChild(addTaskBtn);
-		newBoardSection.appendChild(newBoardTitle);
-		newBoardSection.appendChild(taskForm);
-		// // eslint-disable-next-line no-magic-numbers
-		// for(let i = 0; i < 3; i++){
-		// 	const taskSection = document.createElement('section');
 
-		// 	taskSection.id = `taskList${i.toString()}`;
-		// 	taskSection.classList.add('taskSection');
+		boardInfoContainer.appendChild(newBoardTitle);
+		boardInfoContainer.appendChild(taskForm);
 
-
-		// 	newBoardSection.appendChild(taskSection);
-		// }
+		newBoardSection.appendChild(boardInfoContainer);
 
 		mainPage.appendChild(newBoardSection);
 		addTaskBtn.addEventListener('click', () => this.addNewTask(this.tasks));
@@ -64,7 +57,7 @@ export class Board  {
 			const taskSection = document.createElement('section');
 
 			taskSection.id = `taskList${i.toString()}`;
-			taskSection.classList.add('taskSection');
+			taskSection.classList.add('taskSection','normalState');
 			boardSection.appendChild(taskSection);
 
 			this.handleDragEvents(taskSection);
@@ -85,7 +78,8 @@ export class Board  {
 
 	private handleDragEvents(taskSection: HTMLElement){
 
-		taskSection.addEventListener('dragover', (e: Event) => {
+		taskSection.addEventListener('dragover', (e: any) => {
+			taskSection.classList.replace('normalState','draggedOver');
 			e.preventDefault();
 		});
 
@@ -93,10 +87,15 @@ export class Board  {
 			e.preventDefault();
 		});
 
+		taskSection.addEventListener('dragleave', () => {
+			taskSection.classList.replace('draggedOver', 'normalState');
+		});
+
 		taskSection.addEventListener('drop', (e: any) => {
 			e.preventDefault();
 
 			const draggedItem = e.dataTransfer.getData('text');
+			taskSection.classList.replace('draggedOver','normalState');
 			taskSection.appendChild(<HTMLDivElement>document.querySelector(`#${draggedItem}`));
 			e.dataTransfer.clearData();
 		});

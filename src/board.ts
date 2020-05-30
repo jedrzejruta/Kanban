e1bf984931d631669cfc22a0d4ec9fd9964aad5b
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-05-30 02:02:14 +0200

    add task lists for each board add task removal add drag and drop event handling

--- src/board.ts
+++ src/board.ts
@@ -7,25 +7,26 @@ export class Board  {
 	constructor(public boardName: string) {
 		this.boardID = Date.now().toString();
 		this.addNewBoard(boardName);
-		
-		// this.addNewTask();
 	}
 
 	private addNewBoard(_boardName: string): void{
-
+	// TODO, W trakcie, Zrobione
 		const mainPage: HTMLElement = <HTMLElement>document.querySelector('main'),
+			taskForm: HTMLFormElement = document.createElement('form'),
 			addTaskBtn: HTMLButtonElement = document.createElement('button'),
 			newBoardSection: HTMLElement = document.createElement('section'),
 			newBoardTitle: HTMLHeadingElement = document.createElement('h1'),
 			taskNameInput: HTMLInputElement = document.createElement('input'),
 			taskDescInput: HTMLTextAreaElement = document.createElement('textarea');
-		
+
+		taskForm.setAttribute('action','javascript:;');
+
 		taskNameInput.setAttribute('type', 'text');
 		taskNameInput.setAttribute('placeholder','Task list name');
 		taskNameInput.setAttribute('pattern','.*\\S.*');
 		taskNameInput.required = true;
 		taskNameInput.classList.add('taskListName');
-
+		
 		taskDescInput.setAttribute('placeholder','Write your note here');
 		taskDescInput.classList.add('taskListDesc');
 
@@ -36,27 +37,68 @@ export class Board  {
 
 		newBoardSection.classList.add('board','active');
 		newBoardSection.id = this.boardID;
+		taskForm.appendChild(taskNameInput);
+		taskForm.appendChild(taskDescInput);
+		taskForm.appendChild(addTaskBtn);
 		newBoardSection.appendChild(newBoardTitle);
-		newBoardSection.appendChild(taskNameInput);
-		newBoardSection.appendChild(taskDescInput);
-		newBoardSection.appendChild(addTaskBtn);
-		
+		newBoardSection.appendChild(taskForm);
+		// // eslint-disable-next-line no-magic-numbers
+		// for(let i = 0; i < 3; i++){
+		// 	const taskSection = document.createElement('section');
+
+		// 	taskSection.id = `taskList${i.toString()}`;
+		// 	taskSection.classList.add('taskSection');
+
+
+		// 	newBoardSection.appendChild(taskSection);
+		// }
+
 		mainPage.appendChild(newBoardSection);
 		addTaskBtn.addEventListener('click', () => this.addNewTask(this.tasks));
+		this.createListSections(newBoardSection);
 	}
 
-	private addNewTask(tasks: Array<Task>): void{
+	private createListSections(boardSection: HTMLElement): void{
+		// eslint-disable-next-line no-magic-numbers
+		for(let i = 0; i < 3; i++){
+			const taskSection = document.createElement('section');
+
+			taskSection.id = `taskList${i.toString()}`;
+			taskSection.classList.add('taskSection');
+			boardSection.appendChild(taskSection);
+
+			this.handleDragEvents(taskSection);
+		}	
+	}
+
+	private addNewTask(tasks: Array<Task>): void {
 		const activeBoard: HTMLElement = <HTMLElement>document.querySelector('.active'),
 			taskName: string = (<HTMLInputElement>activeBoard.querySelector('.taskListName')).value.trim(),
 			taskDesc: string = (<HTMLTextAreaElement>activeBoard.querySelector('.taskListDesc')).value;
 
 		if(taskName.length !== 0) {
-			const task: Task = new Task(taskName,taskDesc);
+			const task: Task = new Task(taskName, taskDesc);
 			tasks.push(task);
 		}
 		else return;
-		// addtask.addEventListener('click', () => {
-		// });
 	}
-	
+
+	private handleDragEvents(taskSection: HTMLElement){
+
+		taskSection.addEventListener('dragover', (e: Event) => {
+			e.preventDefault();
+		});
+
+		taskSection.addEventListener('dragenter', (e: Event) => {
+			e.preventDefault();
+		});
+
+		taskSection.addEventListener('drop', (e: any) => {
+			e.preventDefault();
+
+			const draggedItem = e.dataTransfer.getData('text');
+			taskSection.appendChild(<HTMLDivElement>document.querySelector(`#${draggedItem}`));
+			e.dataTransfer.clearData();
+		});
+	}
 }
--- src/task.ts
+++ src/task.ts
@@ -1,26 +1,59 @@
 export class Task {
 	constructor(taskName: string, taskDesc: string) {
-		const activeBoard: HTMLElement = <HTMLElement>document.querySelector('.active'); //append task to active board 
+		const activeBoard: HTMLElement = <HTMLElement>document.querySelector('.active'),
+			toDoTaskSection: HTMLElement = <HTMLElement>activeBoard.querySelector('#taskList0'); // append task to active board 
 
 		console.log('dziala');
 		const newTaskEl: HTMLDivElement = document.createElement('div'),
 			newTaskName: HTMLHeadingElement = document.createElement('h1'),
-			newTaskDesc: HTMLParagraphElement = document.createElement('p');
+			newTaskDesc: HTMLParagraphElement = document.createElement('p'),
+			newTaskDelButton: HTMLButtonElement = document.createElement('button'),
+			taskID: string = 't' + Date.now().toString();
 
-		newTaskName.innerText = taskName;
-		newTaskDesc.innerText = taskDesc;
+		newTaskName.textContent = taskName;
+		newTaskDesc.textContent = taskDesc;
 		newTaskEl.draggable = true;
+		newTaskEl.id = taskID;
 		newTaskEl.classList.add('note');
 		newTaskName.classList.add('noteBar');
 
+		newTaskDelButton.textContent = 'Delete task'; 
+
 		newTaskEl.appendChild(newTaskName);
 		newTaskEl.appendChild(newTaskDesc);
-		activeBoard.appendChild(newTaskEl);
+		newTaskEl.appendChild(newTaskDelButton);
+		toDoTaskSection.appendChild(newTaskEl);
+
+		newTaskDelButton.addEventListener('click',() => {
+			this.deleteTask(newTaskEl);
+		});
+		this.handleTaskDrag(newTaskEl);
+	}
+
+	handleTaskDrag(taskEl: HTMLDivElement): void {
+
+		taskEl.addEventListener('dragstart', (e: any) => {
+			e.dataTransfer.setData('text/plain', e.target.id);
+			setTimeout(() => {
+				taskEl.style.display = 'none';
+			}, 0);
+		});
+
+		taskEl.addEventListener('dragend', () => {
+			setTimeout(() => {
+				taskEl.style.display = 'block';
+			}, 0);
+		});
+	}
+
+	deleteTask(taskEl: HTMLDivElement): void{
+		console.log(taskEl);
+		taskEl.remove();
 	}
 }
 // window.onclick = (e : Event) => {
 // 	console.log(e.target);
 
 // }; 
-//TODO:
-//Task list contains notes, add notes, drag and drop
+// TODO:
+// Task list contains notes, add notes, drag and drop
